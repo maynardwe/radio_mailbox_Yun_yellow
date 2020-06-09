40b752dc09d546295f7d1170a7471cd4dd473437
maynardwe <56454345+maynardwe@users.noreply.github.com>
2020-06-08 21:08:04 -0400

    Add Annie's phone for text & make send_text func

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,40 @@ const int GREEN = 5;
 int numRuns = 1;  // Execution count, so this doesn't run forever
 int maxRuns = 10; // Maximum number of times the Choreo should be executed
 
+void send_text(String phone_to)
+{
+  Serial.println("Sending Text To " + phone_to + " - Run #" + String(numRuns++));
+  TembooChoreo SendSMSChoreo;
+
+  // Invoke the Temboo client
+  SendSMSChoreo.begin();
+
+  // Set Temboo account credentials
+  SendSMSChoreo.setAccountName(TEMBOO_ACCOUNT);
+  SendSMSChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
+  SendSMSChoreo.setAppKey(TEMBOO_APP_KEY);
+
+  // Set Choreo inputs
+  SendSMSChoreo.addInput("AuthToken", TWILIO_AUTH_TOKEN);   //  AUTH TOKEN
+  SendSMSChoreo.addInput("To", phone_to);                   // cell number that your want the text to go to
+  SendSMSChoreo.addInput("From", TWILIO_PHONE_FROM);        // Twilio phone number
+  SendSMSChoreo.addInput("Body", "You've got mail!");       // Message
+  SendSMSChoreo.addInput("AccountSID", TWILIO_ACCOUNT_SID); // Account SID
+
+  // Identify the Choreo to run
+  SendSMSChoreo.setChoreo("/Library/Twilio/SMSMessages/SendSMS");
+
+  // Run the Choreo; when results are available, print them to serial
+  SendSMSChoreo.run();
+
+  while (SendSMSChoreo.available())
+  {
+    char c = SendSMSChoreo.read();
+    //Serial.print(c);
+  }
+  SendSMSChoreo.close();
+}
+
 void setup()
 {
   Bridge.begin();
@@ -47,37 +81,8 @@ void loop()
 
     if (numRuns <= maxRuns)
     {
-      Serial.println("Sending Text - Running SendSMS - Run #" + String(numRuns++));
-
-      TembooChoreo SendSMSChoreo;
-
-      // Invoke the Temboo client
-      SendSMSChoreo.begin();
-
-      // Set Temboo account credentials
-      SendSMSChoreo.setAccountName(TEMBOO_ACCOUNT);
-      SendSMSChoreo.setAppKeyName(TEMBOO_APP_KEY_NAME);
-      SendSMSChoreo.setAppKey(TEMBOO_APP_KEY);
-
-      // Set Choreo inputs
-      SendSMSChoreo.addInput("AuthToken", TWILIO_AUTH_TOKEN);   //  AUTH TOKEN
-      SendSMSChoreo.addInput("To", TWILIO_PHONE_TO_WILL);       // cell number that your want the text to go to
-      SendSMSChoreo.addInput("From", TWILIO_PHONE_FROM);        // Twilio phone number
-      SendSMSChoreo.addInput("Body", "You've got mail!");       // Message
-      SendSMSChoreo.addInput("AccountSID", TWILIO_ACCOUNT_SID); // Account SID
-
-      // Identify the Choreo to run
-      SendSMSChoreo.setChoreo("/Library/Twilio/SMSMessages/SendSMS");
-
-      // Run the Choreo; when results are available, print them to serial
-      SendSMSChoreo.run();
-
-     while (SendSMSChoreo.available())
-      {
-        char c = SendSMSChoreo.read();
-        //Serial.print(c);
-      }
-      SendSMSChoreo.close();
+      send_text(TWILIO_PHONE_TO_WILL);
+      //send_text(TWILIO_PHONE_TO_ANNIE);
     }
 
     Serial.println("Waiting 20 seconds");
